f47a4e61ab8e8851ba8588f2f71c9d7ebc2364c5
Nathan <nathan@vieiraproenca.com>
2016-06-19 16:28:10 -0300

    Removendo templates vazios

--- codeforces/673/temp.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-template<typename T> void _max(T& a, T b) { a = max(a,b); }
-template<typename T> void _min(T& a, T b) { a = min(a,b); }
-
-int main(){
-}
